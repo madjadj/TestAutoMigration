dcb8d88fef5428f8085dec5cf61567e3a8a8b73d
madjadj <madjadj@yahoo.fr>
2012-07-03 22:32:51 +0200

    Application test automigration tutoriel:http://blog.appharbor.com/2012/04/24/automatic-migrations-with-entity-framework-4-3

---
+++ Core/Persistence/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Entity;
+using Core.Model;
+
+namespace Core.Persistence
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context, Configuration>());
+        }
+
+    }
+}
